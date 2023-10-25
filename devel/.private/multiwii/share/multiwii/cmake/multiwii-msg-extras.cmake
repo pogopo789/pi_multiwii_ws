a44efeabbe432e66c43e7fffdb9a754485ce961f
pogopo789 <pogopo789@gmail.com>
2023-10-25 23:49:18 +0700

    update 25/10

--- devel/.private/multiwii/share/multiwii/cmake/multiwii-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(multiwii_MESSAGE_FILES "/home/thanhdat/pi_multiwii_ws/src/multiwii/msg/MSPRawMessage.msg")
-set(multiwii_SERVICE_FILES "/home/thanhdat/pi_multiwii_ws/src/multiwii/srv/ReceiveMSPRawMessage.srv;/home/thanhdat/pi_multiwii_ws/src/multiwii/srv/SendMSPRawMessage.srv")
--- devel/share/gennodejs/ros/multiwii/msg/MSPRawMessage.js
+++
@@ -1,1 +0,0 @@
-/home/thanhdat/pi_multiwii_ws/devel/.private/multiwii/share/gennodejs/ros/multiwii/msg/MSPRawMessage.js
