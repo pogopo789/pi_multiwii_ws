c4a5433abd8de1702be30d4b2b372f21bc150add
pogopo789 <pogopo789@gmail.com>
2023-10-25 23:42:45 +0700

    catkin_make_isolated

---
+++ devel_isolated/multiwii/share/multiwii/cmake/multiwii-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(multiwii_MESSAGE_FILES "/home/thanhdat/pi_multiwii_ws/src/multiwii/msg/MSPRawMessage.msg")
+set(multiwii_SERVICE_FILES "/home/thanhdat/pi_multiwii_ws/src/multiwii/srv/ReceiveMSPRawMessage.srv;/home/thanhdat/pi_multiwii_ws/src/multiwii/srv/SendMSPRawMessage.srv")
---
+++ devel_isolated/setup.zsh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+# generated from catkin.builder Python module
+
+. "/home/thanhdat/pi_multiwii_ws/devel_isolated/multiwii/setup.zsh"
