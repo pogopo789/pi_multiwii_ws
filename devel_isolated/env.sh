c97f5b9b3fbe108609db699df5794e2cd3d13894
pogopo789 <pogopo789@gmail.com>
2023-10-26 16:37:21 +0700

    test

--- devel_isolated/env.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-# generated from catkin.builder Python module
-
-/pi_multiwii_ws/devel_isolated/multiwii/env.sh "$@"
--- devel_isolated/msp/setup.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# generated from catkin.builder Python module
-
-CATKIN_SHELL=bash
-. "/pi_multiwii_ws/devel_isolated/msp/setup.sh"
