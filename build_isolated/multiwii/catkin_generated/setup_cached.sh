9af9d61faaa3540e193fb066a3e17374f6099eac
pogopo789 <pogopo789@gmail.com>
2023-10-26 13:46:38 +0000

    docker

--- build_isolated/multiwii/catkin_generated/setup_cached.sh
+++ build_isolated/multiwii/catkin_generated/setup_cached.sh
@@ -8,9 +8,9 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH='/pi_multiwii_ws/devel_isolated/multiwii:/opt/ros/noetic:/pi_multiwii_ws/devel_isolated/msp'
-export LD_LIBRARY_PATH='/opt/ros/noetic/lib:/opt/ros/noetic/lib/x86_64-linux-gnu:/pi_multiwii_ws/devel_isolated/msp/lib:/pi_multiwii_ws/devel_isolated/msp/lib/x86_64-linux-gnu'
-export PATH='/opt/ros/noetic/bin:/pi_multiwii_ws/devel_isolated/msp/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/thanhdat/.dotnet/tools'
-export PKG_CONFIG_PATH='/opt/ros/noetic/lib/pkgconfig:/opt/ros/noetic/lib/x86_64-linux-gnu/pkgconfig:/pi_multiwii_ws/devel_isolated/msp/lib/pkgconfig:/pi_multiwii_ws/devel_isolated/msp/lib/x86_64-linux-gnu/pkgconfig'
-export PYTHONPATH='/opt/ros/noetic/lib/python3/dist-packages:/pi_multiwii_ws/devel_isolated/msp/lib/python3/dist-packages'
+export LD_LIBRARY_PATH='/pi_multiwii_ws/devel_isolated/multiwii/lib:/opt/ros/noetic/lib:/pi_multiwii_ws/devel_isolated/msp/lib:/pi_multiwii_ws/devel_isolated/msp/lib/x86_64-linux-gnu'
+export PATH='/opt/ros/noetic/bin:/pi_multiwii_ws/devel_isolated/msp/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+export PKG_CONFIG_PATH='/pi_multiwii_ws/devel_isolated/multiwii/lib/pkgconfig:/opt/ros/noetic/lib/pkgconfig:/pi_multiwii_ws/devel_isolated/msp/lib/pkgconfig:/pi_multiwii_ws/devel_isolated/msp/lib/x86_64-linux-gnu/pkgconfig'
+export PYTHONPATH='/pi_multiwii_ws/devel_isolated/multiwii/lib/python3/dist-packages:/opt/ros/noetic/lib/python3/dist-packages:/pi_multiwii_ws/devel_isolated/msp/lib/python3/dist-packages'
 export ROSLISP_PACKAGE_DIRECTORIES='/pi_multiwii_ws/devel_isolated/multiwii/share/common-lisp'
 export ROS_PACKAGE_PATH="/pi_multiwii_ws/src/multiwii:$ROS_PACKAGE_PATH"
